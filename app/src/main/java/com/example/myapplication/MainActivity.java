813084ffba4dfb001a914b2383e31de0f1c13892
T1W3R <werquin.timothe@gmail.com>
2022-12-15 15:45:23 +0100

    Connexion users

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,21 +10,40 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private Button connect;
     private EditText login;
     private EditText password;
+    private RequestQueue Queue;
+
+    private String mail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         connect = findViewById(R.id.connect);
         login = findViewById(R.id.editTextLogin);
         password = findViewById(R.id.editTextPassword);
-        connect.setOnClickListener(new View.OnClickListener() {
+
+        Queue = Volley.newRequestQueue(this);
+
+        connect.setOnClickListener(new View.OnClickListener(){
 
 
             @Override
@@ -34,18 +52,46 @@ public class MainActivity extends AppCompatActivity {
                 final String login_entry = login.getText().toString();
                 final String pwd_entry = password.getText().toString();
 
-                if (login_entry.equals("test") && pwd_entry.equals("pwd")) {
+                String url = "http://172.16.106.19/SLAM/AP3/AP3/API/connexion.php?login=" + login_entry + "&mdp=" + pwd_entry;
 
-                    Intent Home = new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(Home);
-                    finish();
+               JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                        response -> {
+                            try {
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "Identifiant ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
-                }
+                                mail = response.getString("cl_email");
+                                Log.v("Test", mail);
+                                if(mail != "false") {
+                                    Intent Home= new Intent(MainActivity.this, HomeActivity.class);
+                                    startActivity(Home);
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(), "Identifiant ou mot de passe incorrect", Toast.LENGTH_SHORT).show();
+                                }
+
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }, Throwable::printStackTrace){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        HashMap<String, String> headers = new HashMap<String, String>();
+                        headers.put("CUSTOM_HEADER", "Yahoo");
+                        headers.put("ANOTHER_CUSTOM_HEADER", "Google");
+                        return headers;
+                    }
+                };
+                Queue.add(request);
+
+
             }
+
         });
 
+
     }
+
 
 }
