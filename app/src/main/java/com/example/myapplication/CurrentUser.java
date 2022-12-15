bb00488d990fcc9d62ed250180918cfcfd333571
T1W3R <werquin.timothe@gmail.com>
2022-12-15 16:24:42 +0100

    Creation CurrentUser

---
+++ app/src/main/java/com/example/myapplication/CurrentUser.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class CurrentUser {
+
+    private int id;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String mail;
+
+    public CurrentUser(int id, String nom, String prenom, String adresse, String mail){
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.mail = mail;
+    }
+
+
+
+}
